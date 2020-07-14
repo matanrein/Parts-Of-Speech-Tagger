ecf45b37dd2f2f1baa52b29cda09cc42be9e18db
Matan <matanrein@gmail.com>
2020-07-14 18:46:08 +0200

    refactored generate

--- pos-tagger/constants.py
+++ pos-tagger/constants.py
@@ -1,1 +1,2 @@
 HMM_PICKLE_FILE = 'hmm-model.pkl'
+GENERATED_TAGS_FILE = 'generated-tags-file'
--- pos-tagger/generate.py
+++ pos-tagger/generate.py
@@ -3,29 +3,38 @@ from io import open
 import nltk
 from nltk.stem import WordNetLemmatizer
 import numpy as np
-import pandas as pd
-from constants import HMM_PICKLE_FILE
+from constants import HMM_PICKLE_FILE, GENERATED_TAGS_FILE
 import pickle
+import argparse
+import sys
 
-nltk.download('wordnet')
-lemmatizer = WordNetLemmatizer()
-# load to csv
-CSV_FILE = 'pos-tagger/tests/test-data/test-sentences'
-GENERATED_TAGS_FILE = 'generated-tags-file.txt'
-words = []
-with open(CSV_FILE, 'rt') as text_file:
-    lines = list(csv.reader(text_file))
-lemmatized = [lemmatizer.lemmatize(word) for line in lines for word in line]
-lengths = [len(line) for line in lines]
-X_test = np.array(lemmatized).reshape(-1,1)
-hmm, feature_encoder, tag_encoder = pickle.load(open(HMM_PICKLE_FILE, 'rb'))
-X_test = feature_encoder.transform(X_test)
-Y_pred = hmm.predict(X_test, lengths).reshape(-1, 1)
-pred_tags = tag_encoder.inverse_transform(Y_pred).reshape(-1)
-print(pred_tags.shape)
-split_indices = np.cumsum(lengths)
-tags_by_sentence = np.split(pred_tags, split_indices)
-with open(GENERATED_TAGS_FILE, 'wt') as tags_file:
-    writer = csv.writer(tags_file)
-    writer.writerows(tags_by_sentence)
-print('tags were written to ' + GENERATED_TAGS_FILE)
+
+def generate_tags(csv_file):
+    nltk.download('wordnet')
+    lemmatizer = WordNetLemmatizer()
+    words = []
+    with open(csv_file, 'rt') as text_file:
+        lines = list(csv.reader(text_file))
+    lemmatized = [lemmatizer.lemmatize(word) for line in lines for word in line]
+    lengths = [len(line) for line in lines]
+    X_test = np.array(lemmatized).reshape(-1,1)
+    hmm, feature_encoder, tag_encoder = pickle.load(open(HMM_PICKLE_FILE, 'rb'))
+    X_test = feature_encoder.transform(X_test)
+    Y_pred = hmm.predict(X_test, lengths).reshape(-1, 1)
+    pred_tags = tag_encoder.inverse_transform(Y_pred).reshape(-1)
+    print(pred_tags.shape)
+    split_indices = np.cumsum(lengths)
+    tags_by_sentence = np.split(pred_tags, split_indices)
+    return tags_by_sentence
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='this command evaluates the model trained by the train command')
+    parser.add_argument('text_csv_file')
+    args = parser.parse_args(sys.argv[1:])
+    csv_text_file = args.text_csv_file
+    tags_by_sentence = generate_tags(csv_text_file)
+    with open(GENERATED_TAGS_FILE, 'wt') as tags_file:
+        writer = csv.writer(tags_file)
+        writer.writerows(tags_by_sentence)
+    print('tags were written to ' + GENERATED_TAGS_FILE)
